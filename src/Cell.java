3218cdec1241526c744f554271ad630838dc51fb
chucho2 <chucho2@unm.edu>
2016-09-28 15:22:01 -0600

    Just testing the screen blit of the Cells

--- src/Cell.java
+++ src/Cell.java
@@ -1,5 +1,27 @@
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+
 /**
  * Created by CEP Computer Tech on 9/28/2016.
  */
 public class Cell extends Xform {
+
+    final PhongMaterial cellColor = new PhongMaterial();
+    final Box displayCell = new Box(1,1,1);
+
+    public Cell()
+    {
+        this.getChildren().add(displayCell);
+    }
+    public void setAlive()
+    {
+
+        displayCell.setMaterial(cellColor);
+        cellColor.setSpecularColor(Color.GREEN);
+        cellColor.setDiffuseColor(Color.GREEN);
+    }
+
+
 }
+
